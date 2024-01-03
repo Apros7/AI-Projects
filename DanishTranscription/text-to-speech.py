d21a8a21a5e34e6a2882c3d36f66276c8e0e5c5f
apros7 <lucas.vilsen@gmail.com>
2024-01-03 23:50:50 +0100

    tts works

--- DanishTranscription/text-to-speech.py
+++ DanishTranscription/text-to-speech.py
@@ -4,6 +4,7 @@ import torch
 import soundfile as sf
 from datasets import load_dataset
 import pydub
+from pydub.playback import play
 
 processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
 model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
@@ -12,11 +13,11 @@ vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
 inputs = processor(text="This is a test. I am Aura. I will help you with everything, and in the future I will have arms aswell. I am looking forward to that", return_tensors="pt")
 
 # load xvector containing speaker's voice characteristics from a dataset
-# embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
-# speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
 
-speech = model.generate_speech(inputs["input_ids"], vocoder=vocoder)
+speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
 
 sf.write("speech.wav", speech.numpy(), samplerate=16000)
 sound = pydub.AudioSegment.from_wav("speech.wav")
-pydub.playback.play(sound)
+play(sound)
