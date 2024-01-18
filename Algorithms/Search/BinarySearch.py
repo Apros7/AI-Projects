fc4b5b6f8428465d8c2d410a29ee116831d57ec9
apros7 <lucas.vilsen@gmail.com>
2024-01-18 21:43:09 +0100

    binary search tests

--- Algorithms/Search/BinarySearch.py
+++ Algorithms/Search/BinarySearch.py
@@ -1,13 +1,16 @@
 import numpy as np
 
-def binary_search(arr, v):
+def binary_search(arr : list, v : int):
+    if not isinstance(arr, list) or not isinstance(v, int):
+        raise TypeError("One parameter is not of the correct time")
+        # return False
     # Big O: Log_2(N)
-    rounds = np.log2(len(arr))
-    for _ in rounds:
-        if len(arr) == 1 and v != arrv: return False
+    rounds = int(np.log2(len(arr)))
+    for _ in range(rounds):
+        if len(arr) == 1 and v != arr[0]: return False
         middle = len(arr)//2
         arr1, arrv, arr2 = arr[:middle], arr[middle], arr[middle+1:]
         if v == arrv: return True
-        if v > arrv: arr = arr1
-        arr = arr2
+        if v > arrv: arr = arr2
+        else: arr = arr1
     return False
--- Algorithms/Search/tests.py
+++ Algorithms/Search/tests.py
@@ -1,8 +1,21 @@
 from LinearSearch import linear_search
+from BinarySearch import binary_search
+
+import pytest
 
 def test_linear_search():
     assert linear_search([1, 2, 3, 4, 5, 6], 6)
     assert not linear_search([1, 2, 3, 4, 5, 6], "E")
+    assert not linear_search([1, 2, 3, 4, 5, 6], 8)
+
+def test_binary_search():
+    assert binary_search(list(range(4096)), 56)
+    assert not binary_search(list(range(4096)), 8000)
+    assert binary_search(list(range(4096)), 3011)
+    assert not binary_search(list(range(4096)), -1)
+    with pytest.raises(TypeError):
+        binary_search(list(range(4096)), "E")
 
 if __name__ == "__main__":
     test_linear_search()
+    test_binary_search()
