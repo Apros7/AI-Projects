d0ab1558830b165d6598f79d2188997d6021e1e8
apros7 <lucas.vilsen@gmail.com>
2024-01-20 09:55:15 +0100

    bubble sort implementation

--- Algorithms/Sort/BubbleSort.py
+++ Algorithms/Sort/BubbleSort.py
@@ -1,3 +1,12 @@
 
 # Bubble Sort
 # Slow: (N * (N + 1)) / 2 = O(N^2 + N) = O(N^2)
+
+from typing import List
+
+def bubble_sort(arr : List[int]):
+    for i in reversed(range(0, len(arr))):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
