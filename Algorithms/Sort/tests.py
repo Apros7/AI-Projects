4ec9cfb2b3bcb24904b08427a57c63053d43206c
apros7 <lucas.vilsen@gmail.com>
2024-01-21 12:06:47 +0100

    new queue tests

--- Algorithms/Sort/tests.py
+++ Algorithms/Sort/tests.py
@@ -11,13 +11,13 @@ def test_node():
     assert Node(2, Node(3, Node(5))).next.next.value == 5
 
 def test_queue():
-    queue = Queue([2,5,4,5,2])
-    assert queue.deque() == 2
+    queue = Queue()
+    assert queue.peek() is None
+    assert queue.enqueue(Node(5)) is None
+    queue.enqueue(Node(3))
+    assert len(queue) == 2
     assert queue.peek() == 5
-
-    queue = Queue([2,5,4,5,2])
-    queue.enqueue(3)
-    assert len(queue) == 6
+    assert queue.deque() == Node(5)
 
 if __name__ == "__main__":
     test_bubble_sort()
