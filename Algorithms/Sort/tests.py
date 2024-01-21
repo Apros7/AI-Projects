51ed1af77164ad15c12026f63f7e193e3224cdbe
apros7 <lucas.vilsen@gmail.com>
2024-01-21 11:53:46 +0100

    Queue tests

--- Algorithms/Sort/tests.py
+++ Algorithms/Sort/tests.py
@@ -1,9 +1,19 @@
 from BubbleSort import bubble_sort
+from Queue import Queue
 
 import pytest
 
 def test_bubble_sort():
     assert bubble_sort([1, 3, 7, 4, 2]) == [1, 2, 3, 4, 7]
+
+def test_queue():
+    queue = Queue([2,5,4,5,2])
+    assert queue.deque() == 2
+    assert queue.peek() == 5
+
+    queue = Queue([2,5,4,5,2])
+    queue.enqueue(3)
+    assert len(queue) == 6
 
 if __name__ == "__main__":
     test_bubble_sort()
