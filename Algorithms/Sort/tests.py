af10da3db7543617919c2412e9a727ebc7348bb7
apros7 <lucas.vilsen@gmail.com>
2024-01-22 20:01:06 +0100

    ring buffer

--- Algorithms/Sort/tests.py
+++ Algorithms/Sort/tests.py
@@ -42,3 +42,6 @@ if __name__ == "__main__":
 
 # Benefit of array is search: Only LinearSearch is available for LinkedLists
 # Benefit of LinkedList: Easy to delete and shift (example of a queue) - would perform poorly with array
+# RingBuffer: Array, but if the tail goes to the end, you will just go to the front again i.e implementing a queue in an array
+# Allows for constant time both adding and deleting at head and tail.
+# Object pool: Instead of creating objects for each user, create the object, set the param, use it, hand back to pool to set new param.
