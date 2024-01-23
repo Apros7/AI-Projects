0f0d21acade1a02895adf5ab9c75865f848cf3e1
apros7 <lucas.vilsen@gmail.com>
2024-01-23 18:06:04 +0100

    test

--- Algorithms/Recursion/tests.py
+++ Algorithms/Recursion/tests.py
@@ -1,8 +1,18 @@
 import pytest
+from MazeSolver import Point, solve_maze
 
 def test_maze():
-    maze1 = [
+    maze = [
         "#####E#",
         "#     #",
         "#S#####"
     ]
+    wall = "#"
+    start = Point(x = 1, y = 2)
+    end = Point(5, 0)
+    path = solve_maze(maze, wall, start, end)
+    correct_path = [Point(x=1, y=2), Point(x=1, y=1), Point(x=2, y=1), Point(x=3, y=1), Point(x=4, y=1), Point(x=5, y=1), Point(x=5, y=0)]
+    assert path == correct_path
+
+if __name__ == "__main__":
+    test_maze()
