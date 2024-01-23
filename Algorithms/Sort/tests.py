b4a1c2801353e6f26e46745e422d7ae7bf205b08
apros7 <lucas.vilsen@gmail.com>
2024-01-23 10:29:27 +0100

    js ArrayList

--- Algorithms/Sort/tests.py
+++ Algorithms/Sort/tests.py
@@ -45,3 +45,4 @@ if __name__ == "__main__":
 # RingBuffer: Array, but if the tail goes to the end, you will just go to the front again i.e implementing a queue in an array
 # Allows for constant time both adding and deleting at head and tail.
 # Object pool: Instead of creating objects for each user, create the object, set the param, use it, hand back to pool to set new param.
+# In js ArrayList is used (un/shift is slow, push/pop/get is fast)
