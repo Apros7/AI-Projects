f8211f0bd0e7e59e627c9a7b4bebf25dbaef4d47
apros7 <lucas.vilsen@gmail.com>
2024-01-24 17:22:57 +0100

    cleaner code

--- Algorithms/Sort/tests.py
+++ Algorithms/Sort/tests.py
@@ -47,10 +47,3 @@ if __name__ == "__main__":
     test_queue()
     test_stack()
     test_quicksort()
-
-# Benefit of array is search: Only LinearSearch is available for LinkedLists
-# Benefit of LinkedList: Easy to delete and shift (example of a queue) - would perform poorly with array
-# RingBuffer: Array, but if the tail goes to the end, you will just go to the front again i.e implementing a queue in an array
-# Allows for constant time both adding and deleting at head and tail.
-# Object pool: Instead of creating objects for each user, create the object, set the param, use it, hand back to pool to set new param.
-# In js ArrayList is used (un/shift is slow, push/pop/get is fast)
