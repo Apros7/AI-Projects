e131bbebd128692279411e97915219bd72076f7e
apros7 <lucas.vilsen@gmail.com>
2024-01-28 14:58:44 +0100

    min heap implemented

--- Algorithms/Trees/heap.py
+++ Algorithms/Trees/heap.py
@@ -5,19 +5,68 @@
 # MaxHeap: Max value is root
 # Could be done using nodes and making a tree using parents/childs, but ArrayList is much faster
 
-class MinHep:
+class MinHeap:
     def __init__(self) -> None:
         self.data = []
         self.length = 0
 
     def insert(self, value : int) -> None:
-        pass
+        self.data[self.length] = value
+        self._heapifyUp(self.length)
+        self.length += 1
 
-    def remove(self) -> int:
-        pass
+    def delete(self) -> int | None:
+        if self.length == 0:
+            return None
+
+        out = self.data[0]
+        if self.length == 1:
+            self.data = []
+            return out
+        
+        self.length -= 1
+        self.data[0] = self.data[self.length]
+        self._heapifyDown(0)
+        return out
+
+    def _heapifyDown(self, idx : int) -> None:
+        leftIdx = self._leftChild(idx)
+        rightIdx = self._rightChild(idx)
+        
+        if idx >= len(self.data) or leftIdx >= len(self.data):
+            return None
+
+        rightValue = self.data[rightIdx]
+        leftValue = self.data[leftIdx]
+        value = self.data[idx]
+
+        if leftValue > rightValue and value > rightValue:
+            self.data[idx], self.data[rightIdx] = self.data[rightIdx], self.data[idx]
+            self._heapifyDown(rightIdx)
+
+        elif rightValue > leftValue and value > leftValue:
+            self.data[idx], self.data[leftIdx] = self.data[leftIdx], self.data[idx]
+            self._heapifyDown(leftIdx)
+
+    def _heapifyUp(self, idx : int) -> None:
+        if idx == 0: 
+            return None
+        parent = self._parent(idx)
+        parentValue = self.data[parent]
+        value = self.data[idx]
+
+        if parentValue > value:
+            self.data[parent], self.data[idx] = self.data[idx], self.data[parent]
+            self._heapifyUp(parent)
+
+    
 
     def _parent(self, idx : int) -> int:
         return (idx - 1)//2
 
-    def _childs(self, idx : int) -> int:
-        return (2 * idx + 1, 2 * idx + 2)
+    def _leftChild(self, idx : int) -> int:
+        return 2 * idx + 1
+    
+    def _rightChild(self, idx : int) -> int:
+        return 2 * idx + 2
+    
