00a9d3c1b4457d1f6b3c5b399a733ac1bfc11783
apros7 <lucas.vilsen@gmail.com>
2024-01-31 16:36:40 +0100

    LRU done without trimming

--- Algorithms/Maps/LRU.py
+++ Algorithms/Maps/LRU.py
@@ -12,18 +12,26 @@ class LRU:
     head : Node | None
     prev : Node | None
 
-    lookup: dict
-    reverse_lookup : dict
+    lookup: Dict[Hashable, Node]
+    reverse_lookup: Dict[Node, Hashable]
 
     def __init__(self, capacity : int = 10) -> None:
         self.length = 0
         self.head = self.tail = None
-        lookup: Dict[Hashable, Node]
-        reverse_lookup: Dict[Node, Hashable]
+        lookup = Dict[Hashable, Node]
+        reverse_lookup = Dict[Node, Hashable]
         self.capacity = capacity
 
     def update(self, key, value : int) -> None:
-        pass
+        node = self.lookup.get(key)
+        if node is None:
+            node = Node(value = value)
+            self.length += 1
+            self._prepend(node)
+            self._trimCache()
+        else:
+            self._detach(node)
+            self._prepend(node)
 
     def get(self, key) -> int | None:
         node = self.lookup.get(key)
@@ -33,8 +41,29 @@ class LRU:
         self._prepend(node)
         return node.value
 
-    def _detach(self, node):
-        pass
+    def _detach(self, node : Node) -> None:
+        if node.prev:
+            node.prev.next = node.next
 
-    def _prepend(self, node):
+        if node.next:
+            node.next.prev = node.prev
+
+        if self.head == node:
+            self.head = self.head.next
+
+        if self.tail == node:
+            self.tail = self.tail.prev
+
+        node.prev = None
+        node.next = None
+
+    def _prepend(self, node : Node) -> None:
+        if self.head == None:
+            self.head = self.tail = node
+            return
+        node.next = self.head
+        self.head.prev = node
+        self.head = node
+    
+    def _trimCache(self) -> None:
         pass
